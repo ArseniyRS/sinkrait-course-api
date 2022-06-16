567b1ae59568a5b4c5786d94fd1572b8990b78a0
Arseniy <mrsinkrait@gmail.com>
2022-06-16 22:32:11 +0600

    feat: add exeption service

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import {Server} from 'http';
+import { ExeptionFilter } from './errors/exeption.filter';
 import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/users.controller';
 export class App{
@@ -8,21 +9,24 @@ export class App{
   server: Server | undefined;
   logger: LoggerService;
   userController: UserController;
-  
+  exeptionFilter: ExeptionFilter;
+
   constructor(logger: LoggerService,
     userController: UserController,
+    exeptionFilter: ExeptionFilter,
     ){
     this.app = express();
     this.port = 8000;
     this.logger = logger;
     this.userController = userController;
+    this.exeptionFilter = exeptionFilter
   }
 
   useRoutes(){
     this.app.use('/users', this.userController.router);
   }
   useExeptionFilters(){
-
+    this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
   }
   public async init(){
     this.useRoutes()
--- src/errors/exeption.filter.ts
+++ src/errors/exeption.filter.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { LoggerService } from "../logger/logger.service";
 import { IExceptionFilter } from "./exeption.filter.interface";
+import { HttpError } from "./http-error.class";
 
 export class ExeptionFilter implements IExceptionFilter{
   logger: LoggerService
@@ -9,8 +10,13 @@ export class ExeptionFilter implements IExceptionFilter{
   }
 
 
-  catch(err: Error, req: Request, res: Response, next: NextFunction){
-    this.logger.error(`${err.message}`);
-    res.status(500).send({error: err.message});
+  catch(err: HttpError | Error, req: Request, res: Response, next: NextFunction){
+    if(err instanceof HttpError){
+      this.logger.error(`[${err.context} | ${err.statusCode}]: ${err.message}`);
+      res.status(err.statusCode).send({error: err.message});
+    }else{
+      this.logger.error(`${err.message}`);
+      res.status(500).send({error: err.message});
+    }
   }
 }
